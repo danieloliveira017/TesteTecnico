ab616c58e96846a08fd7f0a06edc0da575b395b3
danieloliveira017 <88690092+danieloliveira017@users.noreply.github.com>
2024-09-30 20:36:58 -0300

    Novo contexto

---
+++ CompraApi/Migrations/20240930233534_CompraApi.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompraApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompraApi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Condicoes_Produtos_ProdutoId",
+                table: "Condicoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Condicoes_ProdutoId",
+                table: "Condicoes");
+
+            migrationBuilder.DropColumn(
+                name: "ProdutoId",
+                table: "Condicoes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProdutoId",
+                table: "Condicoes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Condicoes_ProdutoId",
+                table: "Condicoes",
+                column: "ProdutoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Condicoes_Produtos_ProdutoId",
+                table: "Condicoes",
+                column: "ProdutoId",
+                principalTable: "Produtos",
+                principalColumn: "Codigo",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
